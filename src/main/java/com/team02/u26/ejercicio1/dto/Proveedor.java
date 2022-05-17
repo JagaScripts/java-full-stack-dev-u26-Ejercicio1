f34c9a528bdc0cd8aa7b2a489942ae48a1970a23
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-17 13:17:00 +0200

    Agregados a los dto to string

--- src/main/java/com/team02/u26/ejercicio1/dto/Proveedor.java
+++ src/main/java/com/team02/u26/ejercicio1/dto/Proveedor.java
@@ -72,6 +72,11 @@ public class Proveedor {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	@Override
+	public String toString() {
+		return "Proveedor [id=" + id + ", nombre=" + nombre + "]";
+	}
 	
 	
 	
