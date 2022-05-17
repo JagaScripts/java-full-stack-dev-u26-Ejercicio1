662b4179c9ddbde4a69faf614028d78062eda213
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-17 10:02:58 +0200

    Creado y configurado el archivo aplication.propierties se conecta ala base de datos y agregado data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,103 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 192.168.1.12    Database: piezas_y_proveedores
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `piezas`
+--
+
+DROP TABLE IF EXISTS `piezas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `piezas` (
+  `codigo` int NOT NULL,
+  `nombre` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `piezas`
+--
+
+LOCK TABLES `piezas` WRITE;
+/*!40000 ALTER TABLE `piezas` DISABLE KEYS */;
+INSERT INTO `piezas` VALUES (1,'Batería'),(2,'Bombillas'),(3,'Discos de frenos'),(4,'Parabrisas'),(5,'Indicador de presión de neumáticos'),(6,'Faros delanteros'),(7,'Bujías'),(8,'Mando de llave a distancia'),(9,'Tiradores de puertas y seguros de cierre'),(10,'Fusibles');
+/*!40000 ALTER TABLE `piezas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proveedores` (
+  `id` char(4) NOT NULL,
+  `nombre` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proveedores`
+--
+
+LOCK TABLES `proveedores` WRITE;
+/*!40000 ALTER TABLE `proveedores` DISABLE KEYS */;
+INSERT INTO `proveedores` VALUES ('000A','Tiendas Aurgi'),('000B','Autoexpress'),('000C','Totcar'),('000D','Oscaro'),('000E','Recambios-expres'),('000F','Norauto'),('000G','Rexbo'),('000H','Piezascochesonline'),('000I','Dosochoauto'),('001J','Recambioclasico');
+/*!40000 ALTER TABLE `proveedores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `suministra`
+--
+
+DROP TABLE IF EXISTS `suministra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `suministra` (
+  `codigo_pieza` int NOT NULL,
+  `id_proveedor` char(4) NOT NULL,
+  `precio` int DEFAULT NULL,
+  PRIMARY KEY (`codigo_pieza`,`id_proveedor`),
+  KEY `id_proveedor` (`id_proveedor`),
+  CONSTRAINT `suministra_ibfk_1` FOREIGN KEY (`codigo_pieza`) REFERENCES `piezas` (`codigo`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `suministra_ibfk_2` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suministra`
+--
+
+LOCK TABLES `suministra` WRITE;
+/*!40000 ALTER TABLE `suministra` DISABLE KEYS */;
+INSERT INTO `suministra` VALUES (1,'000A',75),(2,'000B',15),(3,'000C',300),(4,'000D',20),(5,'000E',60),(6,'000F',45),(7,'000G',15),(8,'000H',55),(9,'000I',100),(10,'001J',35);
+/*!40000 ALTER TABLE `suministra` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-17 10:01:49
