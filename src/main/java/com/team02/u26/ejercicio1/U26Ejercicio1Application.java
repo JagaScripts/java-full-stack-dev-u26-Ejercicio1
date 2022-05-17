3b18f5b1b612c7496645a1039c1e4afcf32d736e
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-17 09:26:12 +0200

    Creado proyecto eclipse con dependencias necesarias

---
+++ src/main/java/com/team02/u26/ejercicio1/U26Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package com.team02.u26.ejercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U26Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U26Ejercicio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/team02/u26/ejercicio1/U26Ejercicio1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.team02.u26.ejercicio1;
+
+import org.junit.jupiter.api.Test;
+
+
+class U26Ejercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
